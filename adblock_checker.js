9706c035c8bfd87a70e1b22b057ed2aad5732acc
CoopGroup <helloworldstudioelem+coopsmallwood@gmail.com>
2020-03-15 19:18:05 +0000

    adguard maybye

--- adblock_checker.js
+++ adblock_checker.js
@@ -2,3 +2,14 @@ if(!document.getElementById('icWrDJsGRnTp')){
   $('#AdblockModal').modal('show')
   
 }
+
+$(document).ready(function(){
+    if($("#wrapfabtest").height() > 0) {
+        
+        
+    } else {
+         $('#AdblockModal').modal('show')
+  
+        
+    }
+});
