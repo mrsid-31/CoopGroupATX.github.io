81c9d92e489044eb5900a4a8b2fb7a4aa56f9949
CoopGroup <helloworldstudioelem+coopsmallwood@gmail.com>
2020-03-16 17:16:13 +0000

    wmll

--- adblock_checker.js
+++ adblock_checker.js
@@ -2,14 +2,3 @@ if(!document.getElementById('icWrDJsGRnTp')){
   $('#AdblockModal').modal('show')
   
 }
-
-$(document).ready(function(){
-    if($("#wrapfabtest").height() > 0) {
-        
-        
-    } else {
-         $('#AdblockModal').modal('show')
-  
-        
-    }
-});
